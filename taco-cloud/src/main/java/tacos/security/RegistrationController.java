017ca29d5e238fceb637b7a37cbc2b9166601bbb
LucaFinizio <51985003+LucaFinizio@users.noreply.github.com>
2019-09-18 10:33:13 +0200

    Fix: Null fields
    Just a workaround for the problem of null fields in the databse (when registering a new user)
    Maybe configuration problem?

--- taco-cloud/src/main/java/tacos/security/RegistrationController.java
+++ taco-cloud/src/main/java/tacos/security/RegistrationController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import tacos.User;
 import tacos.data.UserRepository;
 
 @Controller
@@ -25,7 +27,14 @@ public class RegistrationController {
   
   @PostMapping
   public String processRegistration(RegistrationForm form) {
-    userRepo.save(form.toUser(passwordEncoder));
+	
+	  
+	//BUG: The user is not correctly registered in the database (all fields are null). 
+	//The following two lines are a workaround
+	User utenteDiProva = new User("buzz","infinity","Buzz_Lightyear","via Delli 2","Pescara","Italy","65127","08567876");
+    userRepo.save(utenteDiProva);
+    
+	//userRepo.save(form.toUser(passwordEncoder));
     return "redirect:/login";
   }
 
